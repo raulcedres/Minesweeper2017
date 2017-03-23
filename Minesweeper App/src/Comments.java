9b2300cc8a7edd4eb4f2ab98e265ece6ac8352c9
raulcedres <raul.cedres@upr.edu>
2017-03-22 21:32:24 -0400

    Se añadio clase de commentarios

---
+++ Minesweeper App/src/Comments.java
@@ -0,0 +1,10 @@
+
+public class Comments {
+/* 
+ 1. Commit and Push (User)
+ 2. User hace pull request
+ 3. Owner acepta pull request
+ 4. Owner hace merge
+ 5. Owner hace pull desde eclipse
+ */
+}
