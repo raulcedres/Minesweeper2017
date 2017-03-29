7ec9146d39b9d05e7f6ca142b77f23ddad765d59
DarielJ <DarielJ@DJ>
2017-03-28 21:34:43 -0400

    hola de nuevo

--- Minesweeper App/src/MyPanel.java
+++ Minesweeper App/src/MyPanel.java
@@ -67,7 +67,6 @@ public class MyPanel extends JPanel
 	public void paintComponent(Graphics g) 
 	{
 		super.paintComponent(g);
-
 		//Compute interior coordinates
 
 		Insets myInsets = getInsets();
