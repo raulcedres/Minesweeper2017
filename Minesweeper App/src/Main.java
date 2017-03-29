0822b1768c872440c97b4d0f61b8a734b0a05e0a
raulcedres <raul.cedres@upr.edu>
2017-03-28 20:55:37 -0400

    Final

--- Minesweeper App/src/Main.java
+++ Minesweeper App/src/Main.java
@@ -3,7 +3,7 @@ import javax.swing.JFrame;
 
 public class Main {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Color Grid");
+		JFrame myFrame = new JFrame("Minesweeper");
 		
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setSize(418, 440); 
